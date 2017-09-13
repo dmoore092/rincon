c428d9060b2bcb9f6a30a2fd33920dc6388d2981
Dale Moore <dwmdis@ist-serenity.main.ad.rit.edu>
2017-09-13 13:31:45 -0400

    initial commit with boilerplate code

---
+++ assets/inc/head.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title></title>
+    <link type="text/css" href="assets/css/main.css" rel="stylesheet">
+</head>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include "assets/inc/head.php" ?>
+
+    <body>
+        <?php include "assets/inc/nav.php" ?>
+
+        <main>
+            <p>HTML 5 Boilerplate</p>
+        </main>
+    </body>
+</html> 
